dc7d2efa8d840a6f9a0d8b441446155e2b56bafc
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-09-15 20:55:32 -0700

    Add Arrays lecture

---
+++ Algorithms-Course/6 Arrays.js
@@ -0,0 +1,18 @@
+// How to make an array
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// The other way (Class Array)
+
+let collegeList = new Array('Cornerstone', 'UCW', 'VCC');
+
+// Add values inside the array (PUSH METHOD)
+
+// A method is a function attached to different types od data structures
+
+numbers.push(10);
+collegeList.push('Langara');
+
+// Add values per index
+
+collegeList[4] = 'UBC';

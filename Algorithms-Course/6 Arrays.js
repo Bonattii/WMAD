168abae74864eb475af7a746baa464550c6ac70c
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-09-16 18:44:07 -0700

    Update Arrays lecture

--- Algorithms-Course/6 Arrays.js
+++ Algorithms-Course/6 Arrays.js
@@ -11,8 +11,24 @@ let collegeList = new Array('Cornerstone', 'UCW', 'VCC');
 // A method is a function attached to different types od data structures
 
 numbers.push(10);
-collegeList.push('Langara');
+collegeList.push('Langara'); // Push returns the lenght!!
 
 // Add values per index
 
 collegeList[4] = 'UBC';
+
+// Size of the array
+
+console.log(numbers.length);
+
+// POP method (returns the value that was removed)
+
+numbers.pop();
+console.log(numbers);
+
+// SHIFT method remove the value on the first index
+// UNSHIFT method insert the value on the first index
+// SPLICE method insert a value on any index or remove the value
+// SLICE method returns the part of the array
+
+// Inplace methods changes the original array and Not Inplace don't change the original array and just create a new one (assign to a variable)

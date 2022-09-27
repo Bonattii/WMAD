580743376932c147ffcfeca5c7c44e01b6bb66ad
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-09-26 20:02:17 -0700

    Add Code Exercise 4

---
+++ Algorithms-Course/Exercises/codeExercise4_0926_RodrigoBonatti.js
@@ -0,0 +1,7 @@
+// 1 - the actual is bigger than the last one or small of the last one (For Loop)
+// If the array is sorted
+
+// 2 - If have duplicates in a sorted array
+
+// 3 - if the actual is equal of the last one (2 nested for loops)
+// If have duplicates in a array

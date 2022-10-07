cd02c7ec07c7eee96c7bf9ac5b2b4197b6d01099
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-07 15:32:29 -0700

    Finish Code Exercise1

---
+++ Object Oriented Programming With Java/exercises/Code Exercise1/codeExercise1_1007_RodrigoBonatti.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/**
+ * codeExercise1_1007_RodrigoBonatti
+ */
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+
+    int number = myObj.nextInt();
+    number *= 10;
+
+    System.out.println("Result: " + number);
+  }
+}

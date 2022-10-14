ebdebb94901d436f19df2c6463775547429400de
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-13 19:51:25 -0700

    Add LeetCode folder

---
+++ ProgrammingQuestions/miscellanous.js
@@ -0,0 +1,17 @@
+// Question 1 - Write a function wich takes a parameter as an array
+// where the values inside the array are from 0 ---> n, and you
+// have to find a missing number
+
+let array = [1, 4, 5, 2, 6, 8, 0, 10, 9, 7]; // 3 is missing
+
+function findMissingNumber(arr) {
+  //return missing number
+}
+
+findMissingNumber(array);
+
+// Question 2 - Given an aray of nums and a target , return indices
+// of the two numbers such their sum is the target
+
+// Question 3 - given strings s and t return true if t is an anagram
+// of s, and false otherwise

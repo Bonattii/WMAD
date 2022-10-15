920e3923992dec83185afdf4ee4ce727cbd98ab6
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-14 18:49:13 -0700

    Leet Code day2

--- ProgrammingQuestions/LeetCode/Day2.js
+++ ProgrammingQuestions/LeetCode/Day2.js
@@ -1,2 +1,34 @@
 // Question 2 (FRIDAY) - Given an aray of nums and a target , return indices
 // of the two numbers such their sum is the target
+
+// Brute force solution
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// Better solution
+
+var twoSum = function (nums, target) {
+  let myObj = {};
+  let indexArr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (myObj[diff] !== undefined) {
+      indexArr.push(i);
+      indexArr.push(myObj[diff]);
+    }
+
+    myObj[nums[i]] = i;
+  }
+
+  return indexArr;
+};

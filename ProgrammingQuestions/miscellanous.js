b9c4a0bd6ceddce90a1ba05b708adce6d4112d47
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-18 10:36:46 -0700

    Leet Code day4

--- ProgrammingQuestions/miscellanous.js
+++
@@ -1,7 +0,0 @@
-// Question 1 (THURSDAY) - Write a function wich takes a parameter as an array
-
-// Question 2 (FRIDAY) - Given an aray of nums and a target , return indices
-// of the two numbers such their sum is the target
-
-// Question 3 (MONDAY) - given strings s and t return true if t is an anagram
-// of s, and false otherwise

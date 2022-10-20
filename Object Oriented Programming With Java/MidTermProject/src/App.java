0c430bba6f92e1e3160efce5e5540291394b0efd
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-19 18:53:23 -0700

    Change if statement to switch statement, take care of scenarios, and finish

--- Object Oriented Programming With Java/MidTermProject/src/App.java
+++ Object Oriented Programming With Java/MidTermProject/src/App.java
@@ -8,39 +8,61 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        double number1, number2, result;
+
         System.out.print("Please enter the first number: ");
-        double number1 = sc.nextDouble();
+        while (!sc.hasNextDouble()) {
+            System.out.println("That's not a number!");
+            sc.next();
+        }
+        number1 = sc.nextDouble();
 
         System.out.print("Please enter the second number: ");
-        double number2 = sc.nextDouble();
+        while (!sc.hasNextDouble()) {
+            System.out.println("That's not a number!");
+            sc.next();
+        }
+        number2 = sc.nextDouble();
 
         System.out.print("Please enter the type of the operation(sum - sub - times - div): ");
         String operation = sc.next();
 
-        if (operation == "sum") {
-            Sum sm = new Sum();
-            sm.setNum1(number1);
-            sm.setNum2(number2);
-            double result = sm.calc();
-            System.out.printf("The result is: %.2f", result);
-        } else if (operation == "sub") {
-            Sub sb = new Sub();
-            sb.setNum1(number1);
-            sb.setNum2(number2);
-            double result = sb.calc();
-            System.out.printf("The result is: %.2f", result);
-        } else if (operation == "div") {
-            Div dv = new Div();
-            dv.setNum1(number1);
-            dv.setNum2(number2);
-            double result = dv.calc();
-            System.out.printf("The result is: %.2f", result);
-        } else if (operation == "times") {
-            Times ti = new Times();
-            ti.setNum1(number1);
-            ti.setNum2(number2);
-            double result = ti.calc();
-            System.out.printf("The result is: %.2f", result);
+        switch (operation) {
+            case "sum":
+                Sum sm = new Sum();
+                sm.setNum1(number1);
+                sm.setNum2(number2);
+                result = sm.calc();
+                System.out.printf("The result is: %.2f", result);
+                break;
+            case "sub":
+                Sub sb = new Sub();
+                sb.setNum1(number1);
+                sb.setNum2(number2);
+                result = sb.calc();
+                System.out.printf("The result is: %.2f", result);
+                break;
+            case "times":
+                Times ti = new Times();
+                ti.setNum1(number1);
+                ti.setNum2(number2);
+                result = ti.calc();
+                System.out.printf("The result is: %.2f", result);
+                break;
+            case "div":
+                if (number2 == 0) {
+                    System.out.println("Impossible to divide one number per 0");
+                    break;
+                }
+                Div dv = new Div();
+                dv.setNum1(number1);
+                dv.setNum2(number2);
+                result = dv.calc();
+                System.out.printf("The result is: %.2f", result);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
         }
 
         sc.close();

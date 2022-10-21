187e765aa9ab01c45615cae049b9f89f79073b8f
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-10-20 19:03:17 -0700

    Start calculator assignment

---
+++ Object Oriented Programming With Java/exercises/Calculator/src/App.java
@@ -0,0 +1,61 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("==============================");
+        System.out.println("Type 1 : For Adding");
+        System.out.println("Type 2 : For Subtraction");
+        System.out.println("Type 3 : For Division");
+        System.out.println("==============================");
+
+        Scanner sc = new Scanner(System.in);
+
+        int userChoice = 0;
+        int number1 = 1;
+        int number2 = 1;
+        int output = 0;
+
+        System.out.println("PLease enter a choice : ");
+        try {
+            userChoice = sc.nextInt(); // Waiting for a user to enter the input
+
+            System.out.println("Please enter First number : ");
+            number1 = sc.nextInt();
+
+            System.out.println("Please enter Second number : ");
+            number2 = sc.nextInt();
+
+            while (true) {
+                if (userChoice == 1) {
+                    output = number1 + number2;
+                } else if (userChoice == 2) {
+                    output = number1 - number2;
+                } else if (userChoice == 3) {
+                    output = number1 / number2;
+                }
+
+                System.out.println("Your final output is: " + output);
+                System.out.print("Do you want to use the calculator again? (yes - no) ");
+                String userChoiceContinue = sc.next();
+
+                if (userChoiceContinue == "no") {
+                    break;
+                }
+            }
+
+            System.out.println("Thank you for choosing the calculator, BYE BYE!");
+            // Closing Scanner Class Instance
+            sc.close();
+
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number which is valid meaning integer");
+        } catch (IllegalStateException e) {
+            System.out.println("Sorry, scanner is closed now");
+        } catch (ArithmeticException e) {
+            System.out.println("You cannot divide a number by 0");
+        } catch (Exception e) {
+            System.out.println("Something Went Wrong Sorry");
+        }
+    }
+}
